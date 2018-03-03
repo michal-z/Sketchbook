e391c9b8adfe505eb1b596bbaa3cf512c366675c
michal-z <michalziulek@gmail.com>
2018-03-03 12:52:06 +0100

    Presentation mode for Sketch01

--- Source/Sketch01.cpp
+++ Source/Sketch01.cpp
@@ -4,19 +4,21 @@
 #include <math.h>
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <dxgi1_2.h>
 #include <d2d1_3.h>
+#include <d3d11.h>
+#pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "d3d11.lib")
+using namespace D2D1;
 
 
+#define PRESENTATION_MODE
 #ifdef _DEBUG
 #define ENABLE_DEBUG_LAYER
 #endif
 #define VHR(r) if (FAILED((r))) assert(0);
 #define CRELEASE(c) if ((c)) { (c)->Release(); (c) = nullptr; }
-#define k_Name "Sketch01"
-#define k_ResolutionX 800
-#define k_ResolutionY 800
-#define k_SphereCount 500
 
 struct Sphere
 {
@@ -24,10 +26,19 @@ struct Sphere
 	D2D1_COLOR_F color;
 };
 
+static const char *k_Name = "Sketch02";
+static const unsigned k_ResolutionX = 800;
+static const unsigned k_ResolutionY = 800;
+static const unsigned k_SphereCount = 500;
+static const unsigned k_ScreenX = GetSystemMetrics(SM_CXSCREEN);
+static const unsigned k_ScreenY = GetSystemMetrics(SM_CYSCREEN);
 static double s_Time;
 static float s_DeltaTime;
-static ID2D1Factory6 *s_Factory;
-static ID2D1HwndRenderTarget *s_RenderTarget;
+static ID2D1Device6 *s_D2dDevice;
+static ID2D1DeviceContext6 *s_D2dContext;
+static ID2D1Factory7 *s_D2dFactory;
+static ID2D1Bitmap1 *s_D2dTargetBitmap;
+static IDXGISwapChain1 *s_SwapChain;
 static ID2D1SolidColorBrush *s_FillBrush;
 static ID2D1SolidColorBrush *s_StrokeBrush;
 static Sphere *s_Spheres;
@@ -50,76 +61,90 @@ static inline float Randomf(float begin, float end)
 
 static void Setup()
 {
-	VHR(s_RenderTarget->CreateSolidColorBrush(D2D1::ColorF(0), &s_FillBrush));
-	VHR(s_RenderTarget->CreateSolidColorBrush(D2D1::ColorF(0, 0.125f), &s_StrokeBrush));
+	VHR(s_D2dContext->CreateSolidColorBrush(ColorF(0), &s_FillBrush));
+	VHR(s_D2dContext->CreateSolidColorBrush(ColorF(0, 0.125f), &s_StrokeBrush));
 
 	s_Spheres = new Sphere[k_SphereCount];
 	for (uint32_t i = 0; i < k_SphereCount; ++i)
 	{
-		float x = Randomf(0.0f, k_ResolutionX);
-		float y = Randomf(0.0f, k_ResolutionY);
+		float x = Randomf(0.0f, (float)k_ResolutionX);
+		float y = Randomf(0.0f, (float)k_ResolutionY);
 		float r = Randomf(20.0f, 120.0f);
 
-		s_Spheres[i].shape = D2D1::Ellipse(D2D1::Point2F(x, y), r, r);
-		s_Spheres[i].color = D2D1::ColorF(Randomf(), Randomf(), Randomf(), 0.2f);
+		s_Spheres[i].shape = Ellipse(Point2F(x, y), r, r);
+		s_Spheres[i].color = ColorF(Randomf(), Randomf(), Randomf(), 0.2f);
 	}
 }
 
 static void Draw()
 {
-	s_RenderTarget->BeginDraw();
-	s_RenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+	s_D2dContext->BeginDraw();
+	s_D2dContext->Clear(ColorF(ColorF::White));
+
+#ifdef PRESENTATION_MODE
+	s_D2dContext->SetTransform(Matrix3x2F::Translation(
+		(float)(k_ScreenX / 2 - k_ResolutionX / 2), (float)(k_ScreenY / 2 - k_ResolutionY / 2)));
+	
+	s_D2dContext->PushAxisAlignedClip(
+		RectF(0.0f, 0.0f, (float)k_ResolutionX, (float)k_ResolutionY), D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
+#endif
 
 	for (int i = 0; i < k_SphereCount; ++i)
 	{
 		s_FillBrush->SetColor(s_Spheres[i].color);
-		s_RenderTarget->FillEllipse(s_Spheres[i].shape, s_FillBrush);
-		s_RenderTarget->DrawEllipse(s_Spheres[i].shape, s_StrokeBrush, 3.0f);
+		s_D2dContext->FillEllipse(s_Spheres[i].shape, s_FillBrush);
+		s_D2dContext->DrawEllipse(s_Spheres[i].shape, s_StrokeBrush, 3.0f);
 	}
-	VHR(s_RenderTarget->EndDraw());
+
+#ifdef PRESENTATION_MODE
+	s_D2dContext->PopAxisAlignedClip();
+#endif
+
+	VHR(s_D2dContext->EndDraw());
+	VHR(s_SwapChain->Present(0, 0));
 }
 
 static double GetTime()
 {
-	static LARGE_INTEGER s_Counter0;
-	static LARGE_INTEGER s_Frequency;
-	if (s_Counter0.QuadPart == 0)
+	static LARGE_INTEGER startCounter;
+	static LARGE_INTEGER frequency;
+	if (startCounter.QuadPart == 0)
 	{
-		QueryPerformanceFrequency(&s_Frequency);
-		QueryPerformanceCounter(&s_Counter0);
+		QueryPerformanceFrequency(&frequency);
+		QueryPerformanceCounter(&startCounter);
 	}
 	LARGE_INTEGER counter;
 	QueryPerformanceCounter(&counter);
-	return (counter.QuadPart - s_Counter0.QuadPart) / (double)s_Frequency.QuadPart;
+	return (counter.QuadPart - startCounter.QuadPart) / (double)frequency.QuadPart;
 }
 
 static void UpdateFrameTime(HWND window, double *time, float *deltaTime)
 {
-	static double s_LastTime = -1.0;
-	static double s_LastFpsTime = 0.0;
-	static unsigned s_FrameCount = 0;
+	static double lastTime = -1.0;
+	static double lastFpsTime = 0.0;
+	static unsigned frameCount = 0;
 
-	if (s_LastTime < 0.0)
+	if (lastTime < 0.0)
 	{
-		s_LastTime = GetTime();
-		s_LastFpsTime = s_LastTime;
+		lastTime = GetTime();
+		lastFpsTime = lastTime;
 	}
 
 	*time = GetTime();
-	*deltaTime = (float)(*time - s_LastTime);
-	s_LastTime = *time;
+	*deltaTime = (float)(*time - lastTime);
+	lastTime = *time;
 
-	if ((*time - s_LastFpsTime) >= 1.0)
+	if ((*time - lastFpsTime) >= 1.0)
 	{
-		const double fps = s_FrameCount / (*time - s_LastFpsTime);
+		const double fps = frameCount / (*time - lastFpsTime);
 		const double ms = (1.0 / fps) * 1000.0;
 		char text[256];
 		snprintf(text, sizeof(text), "[%.1f fps  %.3f ms] %s", fps, ms, k_Name);
 		SetWindowText(window, text);
-		s_LastFpsTime = *time;
-		s_FrameCount = 0;
+		lastFpsTime = *time;
+		frameCount = 0;
 	}
-	s_FrameCount++;
+	frameCount++;
 }
 
 static LRESULT CALLBACK ProcessWindowMessage(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
@@ -150,14 +175,20 @@ static HWND MakeWindow()
 	if (!RegisterClass(&winclass))
 		assert(0);
 
-	RECT rect = { 0, 0, k_ResolutionX, k_ResolutionY };
-	if (!AdjustWindowRect(&rect, WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX, 0))
+#ifdef PRESENTATION_MODE
+	DWORD style = WS_POPUP;
+	RECT rect = { 0, 0, (LONG)k_ScreenX, (LONG)k_ScreenY };
+	ShowCursor(FALSE);
+#else
+	DWORD style = WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX;
+	RECT rect = { 0, 0, (LONG)k_ResolutionX, (LONG)k_ResolutionY };
+	if (!AdjustWindowRect(&rect, style, 0))
 		assert(0);
+#endif
 
 	HWND hwnd = CreateWindowEx(
-		0, k_Name, k_Name, WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX | WS_VISIBLE,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		rect.right - rect.left, rect.bottom - rect.top,
+		0, k_Name, k_Name, style | WS_VISIBLE,
+		CW_USEDEFAULT, CW_USEDEFAULT, rect.right, rect.bottom,
 		nullptr, nullptr, nullptr, 0);
 	assert(hwnd);
 
@@ -167,22 +198,58 @@ static HWND MakeWindow()
 #else
 	factoryOptions.debugLevel = D2D1_DEBUG_LEVEL_NONE;
 #endif
-	VHR(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, __uuidof(s_Factory), &factoryOptions, (void **)&s_Factory));
+	VHR(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, __uuidof(s_D2dFactory), &factoryOptions, (void **)&s_D2dFactory));
 
-	D2D1_SIZE_U size = D2D1::SizeU(k_ResolutionX, k_ResolutionY);
+	ID3D11Device *d3d11Device;
+	ID3D11DeviceContext *d3d11Context;
+	D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_1;
+	VHR(D3D11CreateDevice(
+		nullptr, D3D_DRIVER_TYPE_HARDWARE, 0, D3D11_CREATE_DEVICE_BGRA_SUPPORT,
+		&featureLevel, 1, D3D11_SDK_VERSION, &d3d11Device, nullptr, &d3d11Context));
 
-	VHR(s_Factory->CreateHwndRenderTarget(
-		D2D1::RenderTargetProperties(D2D1_RENDER_TARGET_TYPE_DEFAULT, D2D1::PixelFormat(), 96.0f, 96.0f),
-		D2D1::HwndRenderTargetProperties(hwnd, size, D2D1_PRESENT_OPTIONS_IMMEDIATELY),
-		&s_RenderTarget));
+	IDXGIDevice *dxgiDevice;
+	VHR(d3d11Device->QueryInterface(&dxgiDevice));
+
+	VHR(s_D2dFactory->CreateDevice(dxgiDevice, &s_D2dDevice));
+	VHR(s_D2dDevice->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE, &s_D2dContext));
+
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+	swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.Scaling = DXGI_SCALING_NONE;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+
+	IDXGIAdapter *dxgiAdapter;
+	IDXGIFactory2 *dxgiFactory;
+	VHR(dxgiDevice->GetAdapter(&dxgiAdapter));
+	VHR(dxgiAdapter->GetParent(IID_PPV_ARGS(&dxgiFactory)));
+	VHR(dxgiFactory->CreateSwapChainForHwnd(d3d11Device, hwnd, &swapChainDesc, nullptr, nullptr, &s_SwapChain));
+
+	D2D1_BITMAP_PROPERTIES1 bitmapProperties = BitmapProperties1(
+		D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
+		PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE), 96.0f, 96.0f);
+
+	IDXGISurface *dxgiBackBuffer;
+	VHR(s_SwapChain->GetBuffer(0, IID_PPV_ARGS(&dxgiBackBuffer)));
+
+	VHR(s_D2dContext->CreateBitmapFromDxgiSurface(dxgiBackBuffer, &bitmapProperties, &s_D2dTargetBitmap));
+	s_D2dContext->SetTarget(s_D2dTargetBitmap);
+
+	CRELEASE(d3d11Device);
+	CRELEASE(d3d11Context);
+	CRELEASE(dxgiBackBuffer);
+	CRELEASE(dxgiDevice);
+	CRELEASE(dxgiAdapter);
+	CRELEASE(dxgiFactory);
 
 	return hwnd;
 }
 
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	SetProcessDPIAware();
-
 	HWND hwnd = MakeWindow();
 	Setup();
 
@@ -205,7 +272,10 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	delete[] s_Spheres;
 	CRELEASE(s_FillBrush);
 	CRELEASE(s_StrokeBrush);
-	CRELEASE(s_RenderTarget);
-	CRELEASE(s_Factory);
+	CRELEASE(s_D2dFactory);
+	CRELEASE(s_D2dContext);
+	CRELEASE(s_D2dDevice);
+	CRELEASE(s_D2dTargetBitmap);
+	CRELEASE(s_SwapChain);
 	return 0;
 }
